9ad0332a91ae5c0a7a9cda64e77adde7f686e186
MatijaNovosel <mnovosel5@gmail.com>
2020-12-06 23:59:36 +0100

    Tweaks

--- api/src/config/generate-entities.ts
+++ api/src/config/generate-entities.ts
@@ -1,8 +1,5 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
-const { join, extname, basename } = require("path");
-const { readdirSync, renameSync } = require("fs");
-const fs = require("fs");
 
 async function execute() {
   await exec("npx typeorm-model-generator");
