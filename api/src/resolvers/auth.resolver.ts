0ece4d35397f99e4a308962a8bf7ab8c3f97ed8a
MatijaNovosel <mnovosel5@gmail.com>
2020-12-14 17:40:01 +0100

    Auth wip

--- api/src/resolvers/auth.resolver.ts
+++ api/src/resolvers/auth.resolver.ts
@@ -1,15 +1,12 @@
 import { GAppUser } from "./../entities/appuser";
 import { AuthService } from "./../services/auth.service";
 import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import { UseGuards } from "@nestjs/common";
-import { GqlAuthGuard } from "src/auth/guards/gql-auth.guard";
 
 @Resolver()
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   @Mutation(() => GAppUser)
-  @UseGuards(GqlAuthGuard)
   async login(
     @Args({ name: "email" }) email: string,
     @Args({ name: "password" }) password: string
