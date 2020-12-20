66500da79765394eedd40ee1d4f67ccd46d4e9a1
MatijaNovosel <mnovosel5@gmail.com>
2020-12-20 01:01:15 +0100

    Security features and CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+// import csurf from 'csurf';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  // app.use(csurf());
   await app.listen(3000);
 }
 
