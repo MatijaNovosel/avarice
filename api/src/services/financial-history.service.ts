e568fbafa193beace23979696a9aeb5c3ada6d00
MatijaNovosel <mnovosel5@gmail.com>
2020-12-22 21:54:40 +0100

    Filtering by date

--- api/src/services/financial-history.service.ts
+++ api/src/services/financial-history.service.ts
@@ -1,8 +1,17 @@
 import { Financialhistory } from "./../entities/financialhistory";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { MoreThanOrEqual, Repository } from "typeorm";
+import { FindOperator, MoreThanOrEqual, Repository } from "typeorm";
 import { format } from "date-fns";
+
+interface FilterValues {
+  appUserId: number;
+  createdAt?: FindOperator<string>;
+}
+
+interface Filter {
+  where: FilterValues;
+}
 
 @Injectable()
 export class FinancialHistoryService {
@@ -11,17 +20,19 @@ export class FinancialHistoryService {
     private financialHistoryRepository: Repository<Financialhistory>
   ) {}
 
-  async findByUserId(
-    id: number,
-    month: number = 12,
-    year: number = 2020
-  ): Promise<Financialhistory[]> {
-    const data = await this.financialHistoryRepository.find({
+  async findByUserId(id: number, month?: number): Promise<Financialhistory[]> {
+    const filterObject: Filter = {
       where: {
-        appUserId: id,
-        createdAt: MoreThanOrEqual(`${year}-${month}-00 00:00:00`)
+        appUserId: id
       }
-    });
+    };
+    if (month) {
+      filterObject.where.createdAt = MoreThanOrEqual(
+        `2020-${month}-00 00:00:00`
+      );
+    }
+
+    const data = await this.financialHistoryRepository.find(filterObject);
     return data.map((fh) => {
       fh.total = fh.checking + fh.gyro + fh.pocket + fh.euroVal;
       fh.createdAt = format(new Date(fh.createdAt), "dd.MM.yyyy. HH:mm:ss");
