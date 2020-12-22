f1e1e6520bb382c1b0616933b2b51d8434799aa2
MatijaNovosel <mnovosel5@gmail.com>
2020-12-22 21:50:41 +0100

    Date & year filtering

--- api/src/services/financial-history.service.ts
+++ api/src/services/financial-history.service.ts
@@ -1,7 +1,7 @@
 import { Financialhistory } from "./../entities/financialhistory";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { MoreThanOrEqual, Repository } from "typeorm";
 import { format } from "date-fns";
 
 @Injectable()
@@ -11,9 +11,16 @@ export class FinancialHistoryService {
     private financialHistoryRepository: Repository<Financialhistory>
   ) {}
 
-  async findByUserId(id: number): Promise<Financialhistory[]> {
+  async findByUserId(
+    id: number,
+    month: number = 12,
+    year: number = 2020
+  ): Promise<Financialhistory[]> {
     const data = await this.financialHistoryRepository.find({
-      where: { appUserId: id }
+      where: {
+        appUserId: id,
+        createdAt: MoreThanOrEqual(`${year}-${month}-00 00:00:00`)
+      }
     });
     return data.map((fh) => {
       fh.total = fh.checking + fh.gyro + fh.pocket + fh.euroVal;
