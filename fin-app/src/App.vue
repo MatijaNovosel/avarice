9f00ebf777dc2cd864f73d348229396ead6b10bd
Matija Novosel <mnovosel5@gmail.com>
2021-01-17 13:18:26 +0100

    Ripple implementation

--- fin-app/src/App.vue
+++ fin-app/src/App.vue
@@ -15,26 +15,30 @@
             'bg-gray-700': state.currentRoute.name == 'home',
             'bg-gray-600': state.currentRoute.name != 'home'
           }"
-          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3 p-ripple"
+          v-ripple
         >
           <mdi-icon class="mr-3" color="#ffffff" name="home" />
           <span class="select-none">Home</span>
         </div>
         <div
-          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3 p-ripple"
+          v-ripple
         >
           <mdi-icon class="mr-3" color="#ffffff" name="clock" />
           <span class="select-none">History</span>
         </div>
         <div
-          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          class="p-ripple flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          v-ripple
         >
           <mdi-icon class="mr-3" color="#ffffff" name="scale" />
           <span class="select-none">Balances</span>
         </div>
         <div class="flex flex-col border-b border-gray-700 pb-3">
           <div
-            class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+            class="p-ripple flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+            v-ripple
           >
             <mdi-icon class="mr-3" color="#ffffff" name="credit-card-outline" />
             <span class="select-none">Accounts</span>
@@ -42,14 +46,16 @@
         </div>
         <div class="flex flex-col pt-2">
           <div
-            class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+            class="p-ripple flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+            v-ripple
           >
             <mdi-icon class="mr-3" color="#ffffff" name="cog" />
             <span class="select-none">Settings</span>
           </div>
         </div>
         <div
-          class="flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          class="p-ripple flex text-lg items-center cursor-pointer rounded-lg hover:bg-gray-700 py-2 px-3 mx-3"
+          v-ripple
         >
           <mdi-icon class="mr-3" color="#ffffff" name="shield-check" />
           <span class="select-none">Privacy</span>
