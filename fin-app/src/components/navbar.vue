3fbf75f3f5cdadffcab48d06b2bee694c3b180bb
Matija Novosel <mnovosel5@gmail.com>
2021-01-30 20:00:43 +0100

    Context menu fixes ...

--- fin-app/src/components/navbar.vue
+++ fin-app/src/components/navbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="shadow-md bg-white px-8 flex justify-between h-24 z-50 sticky w-full top-0">
+  <div
+    class="shadow-md bg-white px-8 flex justify-between h-24 z-50 sticky w-full top-0 rounded-bl-2xl"
+  >
     <div
       class="flex items-center w-full"
       v-show="state.currentRoute == homeRoute"
@@ -39,12 +41,7 @@
         <span class="text-black ml-4 mr-3 font-bold">
           {{ state.user.displayName }}
         </span>
-        <mdi-icon
-          name="chevron-down"
-          color="#000000"
-          aria-haspopup="true"
-          aria-controls="overlayMenu"
-        />
+        <mdi-icon name="chevron-down" color="#000000" />
       </div>
       <p-menu
         class="rounded-md"
