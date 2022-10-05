704fdf0c59358d7c8de6c013402e78f55798bb55
Matija Novosel <mnovosel5@gmail.com>
2022-10-05 10:55:36 +0200

    Category context menu

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -2,7 +2,10 @@
   <q-page>
     <div class="bg-grey-10 rounded">
       <div class="text-grey-6 q-py-md q-mx-lg flex justify-between items-center">
-        <span> Categories </span>
+        <div class="col">
+          <div>Categories</div>
+          <div class="text-caption text-grey-7">* Right click for more options</div>
+        </div>
         <q-btn class="bg-accent text-white rounded" @click="newCategoryDialogOpen = true">
           New category
         </q-btn>
@@ -14,7 +17,28 @@
         v-model:expanded="expanded"
         dark
         class="bg-dark-2 q-pa-lg rounded-b"
-      />
+      >
+        <template #default-header="prop">
+          <div class="row items-center">
+            <q-icon :name="prop.node.icon" class="q-mr-sm" :style="{ color: prop.node.color }" />
+            {{ prop.node.name }}
+            <q-menu context-menu touch-position>
+              <q-list dense>
+                <q-item clickable>
+                  <q-item-section>
+                    <q-item-label> Delete category </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable>
+                  <q-item-section>
+                    <q-item-label> Add child category </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </div>
+        </template>
+      </q-tree>
     </div>
   </q-page>
   <new-category-dialog v-model:open="newCategoryDialogOpen" />
