40ff0515eebaf5e8e866ab18b033edc5f7e83983
Matija Novosel <mnovosel5@gmail.com>
2023-07-17 10:18:42 +0200

    Removed defineComponent

--- src/components/DrawerItem.vue
+++ src/components/DrawerItem.vue
@@ -14,29 +14,19 @@
   </q-item>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { NavigationRoute } from "src/models/navigation";
-import { defineComponent, PropType } from "vue";
 
-export default defineComponent({
-  name: "drawer-item",
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    caption: {
-      type: String,
-      default: ""
-    },
-    link: {
-      type: Object as PropType<NavigationRoute>,
-      required: true
-    },
-    icon: {
-      type: String,
-      default: ""
-    }
+withDefaults(
+  defineProps<{
+    title: string;
+    caption: string;
+    link: NavigationRoute;
+    icon: string;
+  }>(),
+  {
+    caption: "",
+    icon: ""
   }
-});
+);
 </script>
